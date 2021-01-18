7c8908f5e728b70acd1745cd826b8c58f4acac4c
laopo001 <353272497@qq.com>
2021-01-19 02:18:32 +0800

    fix

--- packages/kelake/src/vnode.rs
+++ packages/kelake/src/vnode.rs
@@ -5,6 +5,7 @@ use std::convert::From;
 pub enum VNode {
     VText(String),
     Int(f64),
+    Bool(bool),
     List(Vec<VNode>),
 }
 impl From<f64> for VNode {
@@ -19,9 +20,19 @@ impl From<&str> for VNode {
     }
 }
 
-// trait ToF64 {
-//     fn to(&self) -> f64;
-// }
+
+impl From<bool> for VNode {
+    fn from(value: bool) -> Self {
+        VNode::Bool(value)
+    }
+}
+
+impl From<Vec<VNode>> for VNode {
+    fn from(value: Vec<VNode>) -> Self {
+        VNode::List(value)
+    }
+}
+
 
 pub fn create_base_element(base_name: &str, props: Value, children: VNode) {}
 
